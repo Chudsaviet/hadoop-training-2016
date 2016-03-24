aa488aa65fd4ba292c6bab9b80280607f2a31284
chudsaviet <chudsaviet@sandbox.hortonworks.com>
2016-03-24 13:13:56 +0000

    Lazy variant updated

--- homework_01/deploy_and_run.sh
+++ homework_01/deploy_and_run.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 set -e
 mvn package
-rm /mnt/hdfs/apps/simple/simple-yarn-app-1.1.0.jar
-cp target/simple-yarn-app-1.1.0.jar /mnt/hdfs/apps/simple/simple-yarn-app-1.1.0.jar || true
-cp target/simple-yarn-app-1.1.0.jar /mnt/hdfs/apps/simple/simple-yarn-app-1.1.0.jar
-yarn jar target/simple-yarn-app-1.1.0.jar com.hortonworks.simpleyarnapp.Client /bin/date 1 hdfs:///apps/simple/simple-yarn-app-1.1.0.jar
+hdfs dfs -rm -skipTrash /apps/simple/simple-yarn-app-1.1.0.jar
+hdfs dfs -copyFromLocal target/simple-yarn-app-1.1.0.jar /apps/simple/simple-yarn-app-1.1.0.jar
+yarn jar target/simple-yarn-app-1.1.0.jar com.hortonworks.simpleyarnapp.Client hdfs:///apps/simple/user.profile.tags.us.txt hdfs:///apps/simple/result 1 hdfs:///apps/simple/simple-yarn-app-1.1.0.jar
