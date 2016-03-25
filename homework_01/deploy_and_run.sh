f215ff26b860bde30b2343fbff3962a96e289ae8
Timofei Korostelev <lincosmail@gmail.com>
2016-03-25 12:13:12 +0000

    Updated deploy process

--- homework_01/deploy_and_run.sh
+++ homework_01/deploy_and_run.sh
@@ -9,4 +9,4 @@ hdfs dfs -rm -skipTrash /apps/tk_hw1/tk-homework1-1.0.jar || true
 hdfs dfs -copyFromLocal target/tk-homework1-1.0-jar-with-dependencies.jar /apps/tk_hw1/tk-homework1-1.0.jar
 hdfs dfs -copyFromLocal user.profile.tags.us.txt /apps/tk_hw1/user.profile.tags.us.txt || true
 yarn jar target/tk-homework1-1.0.jar org.tk.hw1.Client hdfs:///apps/tk_hw1/user.profile.tags.us.txt hdfs:///apps/tk_hw1/result 10 hdfs:///apps/tk_hw1/tk-homework1-1.0.jar
-hdfs dfs -ls -lah /apps/tk_hw1/result
+hdfs dfs -ls /apps/tk_hw1/result
