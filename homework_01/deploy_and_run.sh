33ad0ded31be9921a1522700659ac784de5f9014
Timofei Korostelev <lincosmail@gmail.com>
2016-03-25 11:40:49 +0000

    Updated deploy process

--- homework_01/deploy_and_run.sh
+++ homework_01/deploy_and_run.sh
@@ -4,6 +4,7 @@ set -x
 mvn package
 hdfs dfs -rm -r -skipTrash /apps/tk_hw1/ || true
 hdfs dfs -mkdir /apps/tk_hw1/
+hdfs dfs -chmod a+rwx /apps/tk_hw1/
 hdfs dfs -rm -skipTrash /apps/tk_hw1/tk-homework1-1.0.jar || true
 hdfs dfs -copyFromLocal target/tk-homework1-1.0-jar-with-dependencies.jar /apps/tk_hw1/tk-homework1-1.0.jar
 hdfs dfs -copyFromLocal user.profile.tags.us.txt /apps/tk_hw1/user.profile.tags.us.txt || true
