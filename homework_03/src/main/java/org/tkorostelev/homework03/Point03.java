8ba1f5a89b8dc955d61ef115057d846646e4d5ae
Timofei Korostelev <lincosmail@gmail.com>
2016-04-02 09:04:18 +0300

    Added Configurable and Tool

--- homework_03/src/main/java/org/tkorostelev/homework03/Point03.java
+++ homework_03/src/main/java/org/tkorostelev/homework03/Point03.java
@@ -1,17 +1,22 @@
 package org.tkorostelev.homework03;
 
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.conf.Configured;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.IntWritable;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapreduce.Job;
 import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
 import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
+import org.apache.hadoop.util.Tool;
+import org.apache.hadoop.util.ToolRunner;
 
 import java.net.URI;
 
-public class Point03 {
+public class Point03 extends Configured implements Tool{
 
-    public static void run(String[] args) throws Exception {
+    @Override
+    public int run(String[] args) throws Exception {
         if (args.length != 3) {
             System.err.println("Usage: Point03 <tags_file> <input_path> <output_path>");
             System.exit(-1);
@@ -23,7 +28,9 @@ public class Point03 {
 
         final URI tagsFileURI = new URI(tagsFile);
 
-        Job job = Job.getInstance();
+        final Configuration conf = getConf();
+
+        Job job = Job.getInstance(conf);
         job.setJarByClass(Point03.class);
         job.setJobName("Point03");
 
@@ -39,9 +46,11 @@ public class Point03 {
         job.setOutputValueClass(IntWritable.class);
 
         System.exit(job.waitForCompletion(true) ? 0 : 1);
+        return 0;
     }
 
     public static void main(String[] args) throws Exception {
-        run(args);
+        int exitCode = ToolRunner.run(new Point03(), args);
+        System.exit(exitCode);
     }
 }
